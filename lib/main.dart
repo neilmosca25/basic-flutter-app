274ac3228794939587288660750e16309f173547
neil-mosca <niczm25@gmail.com>
2026-07-03 19:14:31 +0800

    Fetch data from the internet
    Instructions on how to make HTTP requests and parse responses. Learn the MVVM architecture pattern and how to build HTTP requests with async/await.

--- lib/main.dart
+++ lib/main.dart
@@ -23,3 +23,19 @@ class MainApp extends StatelessWidget {
     );
   }
 }
+
+class ArticleModel {
+  Future<Summary> getRandomArticleSummary() async {
+    final uri = Uri.https(
+      'en.wikipedia.org',
+      '/api/rest_v1/page/random/summary',
+    );
+    final response = await get(uri);
+
+    if (response.statusCode != 200) {
+      throw const HttpException('Failed to update resource');
+    }
+
+    return Summary.fromJson(jsonDecode(response.body) as Map<String, Object?>);
+  }
+}
