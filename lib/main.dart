addef164383e472cfffc8f872f61a09313f7a597
neil-mosca <niczm25@gmail.com>
2026-07-03 18:36:08 +0800

    Stateful widgets
    Learn about StatefulWidgets and rebuilding Flutter UI. Learn when widgets need to be stateful and how to trigger UI updates with setState.

--- lib/main.dart
+++ lib/main.dart
@@ -74,30 +74,38 @@ class Tile extends StatelessWidget {
   }
 }
 
-class GamePage extends StatelessWidget {
+class GamePage extends StatefulWidget {
   GamePage({super.key});
-  // This object is part of the game.dart file.
-  // It manages wordle logic, and is outside the scope of this tutorial.
+
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
   final Game _game = Game();
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child:  Column(
-        spacing: 5.0,
+      child: Column(
         children: [
-          for (final guess in _game.guesses)
+          for (var guess in _game.guesses)
             Row(
-              spacing: 5.0,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                for (final letter in guess) Tile(letter.char, letter.type)
+                for (var letter in guess)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 2.5, vertical: 2.5),
+                    child: Tile(letter.char, letter.type),
+                  )
               ],
             ),
           GuessInput(
-            onSubmitGuess: (guess) {
-              // TODO, handle guess
-              print(guess); // Temporary
+            onSubmitGuess: (String guess) {
+               setState(() { // NEW
+                _game.guess(guess);
+              });
             },
           ),
         ],
